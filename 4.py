4d2bbbfcd76fe08296c7228102b262494fcba160
Tyler Degen <degen.19@osu.edu>
2016-11-14 23:31:34 -0500

    Well this should work for 4, just need to clean everything up and add 1 and 2

--- 4.py
+++ 4.py
@@ -8,11 +8,37 @@ R = model.Model("R")
 W = model.CondModel('W')
 O = model.CondModel("O")
 
+OgivR = model.CondModel('OR')
+
 for line in input:
 	R.read(line)
 	W.read(line)
 	O.read(line)
 
 Idata = "I [naek] [nek] [naek]"
+IdataSplit = Idata.split()
 
-RivenIdata = model.Model("RgivI")
+for o in IdataSplit:
+	print(o)
+	
+for r, w in O:
+	for o in O[r,w]:
+		OgivR[r][o] += O[r,w][o] / 2
+		
+for r in OgivR:
+	for o in OgivR[r]:
+		print(o, r, str(OgivR[r][o]))
+
+RgivenIdata = model.Model("RgivI")
+for r in OgivR:
+	for o in OgivR:
+		RgivenIdata[r] 
+		
+RgivenIdata = R
+for o in IdataSplit:
+	for r in RgivenIdata:
+		if OgivR[r][o] != 0:
+			RgivenIdata[r] *= OgivR[r][o]
+		
+for r in RgivenIdata:
+	print (r, RgivenIdata[r])
