57094a144f6b46790ca604c2c7cdc41faeafae5b
Tyler Degen <degen.19@osu.edu>
2016-11-15 00:10:48 -0500

    made some final adjustments to bayesian

--- 4.py
+++ 4.py
@@ -14,24 +14,42 @@ for line in input:
 	R.read(line)
 	W.read(line)
 	O.read(line)
+	
+OgivNone = model.Model("Onone")
 
 Idata = "I [naek] [nek] [naek]"
 IdataSplit = Idata.split()
 	
+#w values aren't given for each time step, so condition out
 for r, w in O:
 	for o in O[r,w]:
 		OgivR[r][o] += O[r,w][o] / 2
+		OgivNone[o] += O[r,w][o] / 4
+
+#for o in OgivNone:
+	#print(o, OgivNone[o])
+
 
 RgivenIdata = model.Model("RgivI")
+
 for r in OgivR:
 	for o in OgivR:
 		RgivenIdata[r] 
-		
+	'''	
 RgivenIdata = R
 for o in IdataSplit:
 	for r in RgivenIdata:
 		if OgivR[r][o] != 0:
 			RgivenIdata[r] *= OgivR[r][o]
+'''
+denominator = 0			
+for r in RgivenIdata:
+	for o in IdataSplit:
+		denominator += OgivR[r][o] * OgivNone[o]
+		
+	numerator = OgivR[r][o] * R[r]
+	RgivenIdata[r] = numerator / denominator
+	denominator = 0
 		
 for r in RgivenIdata:
 	print (r, RgivenIdata[r])
